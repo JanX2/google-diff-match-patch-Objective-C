7102b744060c36bbeedd56cd834338d975e70919
Jan <github@geheimwerk.de>
2011-06-10 19:46:29 +0200

    Rewriting mode-test to support additional modes.

--- Tests/mode-test.m
+++ Tests/mode-test.m
@@ -24,49 +24,88 @@
 
 #import <DiffMatchPatch/DiffMatchPatch.h>
 
+
+typedef enum {
+  DiffDefaultMode = 1,
+  DiffLineMode = 2,
+  DiffWordMode = 3,
+  DiffParagraphMode = 4,
+  DiffSentenceMode = 5,
+  DiffLineBreakAgnosticLineMode = 6
+} DiffMode;
+
+
 NSMutableArray * diff_defaultMode(NSString *text1, NSString *text2);
-NSMutableArray * diff_lineMode(NSString *text1, NSString *text2);
-NSMutableArray * diff_wordMode(NSString *text1, NSString *text2);
+NSMutableArray * diff_withMode(NSString *text1, NSString *text2, DiffMode mode);
 
 NSString * diff_stringForURL(NSURL *aURL);
 
+NSMutableArray * diff_withMode(NSString *text1, NSString *text2, DiffMode mode) {
+  DiffMatchPatch *dmp = [DiffMatchPatch new];
+  
+  DiffTokenMode tokenMode = 0;
+  
+  switch (mode) {
+    case DiffDefaultMode:
+      tokenMode = 0;
+      break;
+    case DiffLineMode:
+      tokenMode = -1;
+      break;
+    case DiffWordMode:
+      tokenMode = DiffWordTokens;
+      break;
+    case DiffParagraphMode:
+      tokenMode = DiffParagraphTokens;
+      break;
+    case DiffSentenceMode:
+      tokenMode = DiffSentenceTokens;
+      break;
+    case DiffLineBreakAgnosticLineMode:
+      tokenMode = DiffLineBreakAgnosticLineTokens;
+      break;
+    default:
+      tokenMode = 0;
+      break;
+  }
+  
+  NSMutableArray *tokenArray;
+  if (tokenMode != 0) {
+    NSArray *a;
+    if (mode == DiffLineMode) {
+      a = [dmp diff_linesToCharsForFirstString:text1 andSecondString:text2];
+    }
+    else {
+      a = [dmp diff_tokensToCharsForFirstString:text1 andSecondString:text2 mode:tokenMode];
+    }
+    
+    NSString *tokenText1 = [a objectAtIndex:0];
+    NSString *tokenText2 = [a objectAtIndex:1];
+    tokenArray = [a objectAtIndex:2];
+    
+    text1 = tokenText1;
+    text2 = tokenText2;
+  }
+  
+  dmp.Diff_Timeout = 0;
+  NSMutableArray *diffs = [dmp diff_mainOfOldString:text1 andNewString:text2 checkLines:NO];
+  
+  if (tokenMode != 0) {
+    [dmp diff_chars:diffs toLines:tokenArray];
+  }
+  
+  [dmp release];
+  
+  return diffs;
+}
+
 NSMutableArray * diff_defaultMode(NSString *text1, NSString *text2) {
-	DiffMatchPatch *dmp = [[DiffMatchPatch new] autorelease];
-	
-	dmp.Diff_Timeout = 0;
-	NSMutableArray *diffs = [dmp diff_mainOfOldString:text1 andNewString:text2 checkLines:NO];
-	
-	return diffs;
-}
-
-NSMutableArray * diff_lineMode(NSString *text1, NSString *text2) {
-	DiffMatchPatch *dmp = [[DiffMatchPatch new] autorelease];
-	
-	NSArray *a = [dmp diff_linesToCharsForFirstString:text1 andSecondString:text2];
-	NSString *lineText1 = [a objectAtIndex:0];
-	NSString *lineText2 = [a objectAtIndex:1];
-	NSMutableArray *lineArray = [a objectAtIndex:2];
-	
-	dmp.Diff_Timeout = 0;
-	NSMutableArray *diffs = [dmp diff_mainOfOldString:lineText1 andNewString:lineText2 checkLines:NO];
-	
-	[dmp diff_chars:diffs toLines:lineArray];
-	return diffs;
-}
-
-NSMutableArray * diff_wordMode(NSString *text1, NSString *text2) {
-	DiffMatchPatch *dmp = [[DiffMatchPatch new] autorelease];
-	
-	NSArray *a = [dmp diff_wordsToCharsForFirstString:text1 andSecondString:text2];
-	NSString *lineText1 = [a objectAtIndex:0];
-	NSString *lineText2 = [a objectAtIndex:1];
-	NSMutableArray *lineArray = [a objectAtIndex:2];
-	
-	dmp.Diff_Timeout = 0;
-	NSMutableArray *diffs = [dmp diff_mainOfOldString:lineText1 andNewString:lineText2 checkLines:NO];
-	
-	[dmp diff_chars:diffs toLines:lineArray];
-	return diffs;
+  DiffMatchPatch *dmp = [[DiffMatchPatch new] autorelease];
+  
+  dmp.Diff_Timeout = 0;
+  NSMutableArray *diffs = [dmp diff_mainOfOldString:text1 andNewString:text2 checkLines:NO];
+  
+  return diffs;
 }
 
 NSString * diff_stringForURL(NSURL *aURL) {
@@ -80,34 +119,30 @@ NSString * diff_stringForURL(NSURL *aURL) {
   if (!attributedString) {
     NSLog(@"%@", error);
   }
+  
   NSString *string = [attributedString string];
 
-  return [string autorelease];
+  [attributedString release];
+  
+  return string;
 }
-
-
-typedef enum {
-  DIFF_DEFAULT_MODE = 1,
-  DIFF_LINE_MODE = 2,
-  DIFF_WORD_MODE = 3
-} Diff_Mode;
 
 
 int main (int argc, const char * argv[]) {
   NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
-	
-	if ([[[NSProcessInfo processInfo] arguments] count] < 3) {
-		fprintf(stderr, "usage: %s <txt1> <txt2> [default|line|word]\n",
+  
+  if ([[[NSProcessInfo processInfo] arguments] count] < 3) {
+    fprintf(stderr, "usage: %s <txt1> <txt2> [default|line|word|paragraph|sentence|line-break-agnostic]\n",
             [[[NSProcessInfo processInfo] processName] UTF8String]);
-		[pool drain];
-		return EXIT_FAILURE;
-	}
+    [pool drain];
+    return EXIT_FAILURE;
+  }
 
-	NSString *rawFilePath1 = [[[NSProcessInfo processInfo] arguments] objectAtIndex:1];
-	NSString *rawFilePath2 = [[[NSProcessInfo processInfo] arguments] objectAtIndex:2];
+  NSString *rawFilePath1 = [[[NSProcessInfo processInfo] arguments] objectAtIndex:1];
+  NSString *rawFilePath2 = [[[NSProcessInfo processInfo] arguments] objectAtIndex:2];
 
   NSURL *fileURL1 = [NSURL fileURLWithPath:rawFilePath1];
-	NSURL *fileURL2 = [NSURL fileURLWithPath:rawFilePath2];
+  NSURL *fileURL2 = [NSURL fileURLWithPath:rawFilePath2];
   
   NSString *text1 = diff_stringForURL(fileURL1);
   NSString *text2 = diff_stringForURL(fileURL2);
@@ -117,33 +152,32 @@ int main (int argc, const char * argv[]) {
   }
   
   NSMutableArray *diffs;
-  NSUInteger mode = DIFF_DEFAULT_MODE;
+  DiffMode mode = DiffDefaultMode;
   
   if ([[[NSProcessInfo processInfo] arguments] count] >= 4) {
     NSString *modeString = [[[NSProcessInfo processInfo] arguments] objectAtIndex:3];
     
     if ([modeString isEqualToString:@"line"]) {
-      mode = DIFF_LINE_MODE;
+      mode = DiffLineMode;
     }
     else if ([modeString isEqualToString:@"word"]) {
-      mode = DIFF_WORD_MODE;
+      mode = DiffWordMode;
+    }
+    else if ([modeString isEqualToString:@"paragraph"]) {
+      mode = DiffParagraphMode;
+    }
+    else if ([modeString isEqualToString:@"sentence"]) {
+      mode = DiffSentenceMode;
+    }
+    else if ([modeString isEqualToString:@"line-break-agnostic"]) {
+      mode = DiffLineBreakAgnosticLineMode;
     }
   }
   
-  switch (mode) {
-    case DIFF_LINE_MODE:
-      diffs = diff_lineMode(text1, text2);
-      break;
-    case DIFF_WORD_MODE:
-      diffs = diff_wordMode(text1, text2);
-      break;
-    default:
-      diffs = diff_defaultMode(text1, text2);
-      break;
-  }
+  diffs = diff_withMode(text1, text2, mode);
   
   NSLog(@"%@", [diffs description]);
   
   [pool drain];
-	return EXIT_SUCCESS;
+  return EXIT_SUCCESS;
 }
